ba2231c820fea5717f9a91bc82f60d733fc6560c
ampetervip <96559063+ampetervip@users.noreply.github.com>
2025-05-16 10:28:47 +0800

    Update softetherbridget_ubuntu.sh

--- softetherbridget_ubuntu.sh
+++ softetherbridget_ubuntu.sh
@@ -6,7 +6,8 @@
 # 版本：v1.1
 
 # 系统环境变量
-IPWAN=$(curl -4 ifconfig.io)  # 设置为固定公网IP
+# IPWAN=$(curl -4 ifconfig.io)  # 设置为固定公网IP
+IPWAN=$(ip -4 addr show eth0 | grep -oP 'inet \K[\d.]+')
 SERVER_IP=$IPWAN
 USER="pi"
 SERVER_PASSWORD="xiaojie"
